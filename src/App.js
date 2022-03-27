30b406fe01ab52aa47ca97c2fc68ff4c75f12b22
fahadsidd107 <fsiddiqui107@gmail.com>
2022-03-28 00:05:26 +0500

    work complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import './App.css'
+import { useSelector, useDispatch } from "react-redux";
+import {incNumber} from "./actions";
+import {decNumber} from "./actions";
 function App() {
+
+  // it alternative to the useContext hooks in react / consumer from context API
+  const MyState = useSelector(state => state.changeTheNumber);
+  const dispatch = useDispatch();
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <h1>Redux Template</h1>
+
+    <div className="main-div">
+      
+
+      <div className="container">
+  
+      <h1>Increment/Decrement counter</h1>
+      <h4>using React and Redux</h4>
+      
+      <div className="quantity">
+        <a className="quantity__minus" title="Decrement" onClick={() => dispatch(decNumber(5))} ><span>-</span></a>
+        <input name="quantity" type="text" className="quantity__input" value={MyState}/>
+        <a className="quantity__plus" title="Increment"  onClick={() => dispatch(incNumber(5))}><span>+</span></a>
+      </div>
+  
+          </div>
+        </div>
+    </>
+  )
 }
 
-export default App;
+export default App
