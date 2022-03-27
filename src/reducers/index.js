79fdd33962b38ff406ed15c7d7be7d6f038a2638
fahadsidd107 <fsiddiqui107@gmail.com>
2022-03-28 00:04:50 +0500

    work complete

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import  changeTheNumber  from './incDec';
+import { combineReducers } from "redux";
+
+const rootReducers = combineReducers(
+    {
+        changeTheNumber
+    }
+);
+
+export default rootReducers;
